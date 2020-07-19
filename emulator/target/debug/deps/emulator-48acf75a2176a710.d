3c3101e8d6d1e2d9b82d6651766449fbb7d560de
Nathan Moses <nathanmoses@pop-os.localdomain>
2020-07-19 20:04:26 +0100

    set up rust environment

---
+++ emulator/target/debug/deps/emulator-48acf75a2176a710.d
@@ -0,0 +1,5 @@
+/home/nathanmoses/workspace/RustGB/emulator/target/debug/deps/emulator-48acf75a2176a710: src/main.rs
+
+/home/nathanmoses/workspace/RustGB/emulator/target/debug/deps/emulator-48acf75a2176a710.d: src/main.rs
+
+src/main.rs:
---
+++ emulator/target/debug/emulator.d
@@ -0,0 +1,1 @@
+/home/nathanmoses/workspace/RustGB/emulator/target/debug/emulator: /home/nathanmoses/workspace/RustGB/emulator/src/main.rs
