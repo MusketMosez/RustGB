4eb174c3c94a07d0677fe147206f64ec461fc74a
Nathan Moses <nathanmoses@pop-os.localdomain>
2020-07-19 21:20:38 +0100

    implemented flags register

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -8,10 +8,11 @@ struct Registers {
    c: u8,
    d: u8,
    e: u8,
-   f: u8,
+   f: FlagsRegister,
    h: u8,
    l: u8,
 }
+
 
 
 
@@ -64,6 +65,55 @@ impl Registers {
 
 
 }
+
+
+//Struct defining Flags register
+//lower 4 bits always 0
+//upper 4 bits represent states
+
+struct FlagsRegister {
+    zero: bool,
+    subtract: bool,
+    half_carry: bool,
+    carry: bool,
+}
+
+
+//Methods to translate from FlagsRegister to u8 and vice-versa
+
+const ZERO_FLAG_BYTE_POSITION: u8 = 7;
+const SUBTRACT_FLAG_BYTE_POSITION: u8 = 6;
+const HALF_CARRY_FLAG_BYTE_POSITION: u8 = 5;
+const CARRY_FLAG_BYTE_POSITION: u8 = 4;
+
+//from FlagsRegister to u8
+impl std::convert::From<FlagsRegister> for u8 {
+    fn from(flag: FlagsRegister) -> u8 {
+        (if flag.zero       { 1 } else { 0 }) << ZERO_FLAG_BYTE_POSITION |
+        (if flag.subtract   { 1 } else { 0 }) << SUBTRACT_FLAG_BYTE_POSITION |
+        (if flag.half_carry { 1 } else { 0 }) << HALF_CARRY_FLAG_BYTE_POSITION |
+        (if flag.carry      { 1 } else { 0 }) << CARRY_FLAG_BYTE_POSITION
+    }
+}
+
+
+//from u8 to FlagsRegister
+impl std::convert::From<u8> for FlagsRegister{
+    fn from(byte: u8) -> Self {
+        let zero = ((byte >> ZERO_FLAG_BYTE_POSITION) & Ob1) != 0;
+        let subtract = ((byte >> SUBTRACT_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let half_carry = ((byte >> HALF_CARRY_FLAG_BYTE_POSITION) & Ob1) != 0;
+        let carry = ((byte >> CARRY_FLAG_BYTE_POSITION) & Ob1) != 0;
+
+        FlagsRegister {
+            zero,
+            subtract,
+            half_carry,
+            carry
+        }
+    }
+}
+
 
 
 
